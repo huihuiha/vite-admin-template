e3b25445620d5f33a3bba500d2f5e1ccd156a2c5
linguanghui <1476465526@qq.com>
2022-11-08 16:47:40 +0800

    feat: 配置eslint规则

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+//.eslintrc.js
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["@vue/prettier", "plugin:vue/vue3-essential", "eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["vue"],
+  rules: {
+    "prettier/prettier": "warn",
+  },
+};
