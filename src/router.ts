f87722ea7b4eda3b9ff860d240ad29834dca85cd
linguanghui <1476465526@qq.com>
2022-11-24 16:17:18 +0800

    feat: 引入路由进度条

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 import routes from "~pages";
+import NProgress from "nprogress"; // progress bar
+import "nprogress/nprogress.css"; // progress bar style
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  next();
+});
+
+router.afterEach(() => {
+  // finish progress bar
+  NProgress.done();
 });
 
 export default router;
